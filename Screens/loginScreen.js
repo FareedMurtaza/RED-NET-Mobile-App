5b9728a9ada4ea28deea9da3487c8e2e2db92d24
Fareed_Murtaza <fareedmurtaza91@gmail.com>
2020-06-26 17:55:02 +0500

    Language

--- Screens/loginScreen.js
+++ Screens/loginScreen.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Image, Text, TextInput, KeyboardAvoidingView, CheckBox, Button, StatusBar } from 'react-native';
+import { StyleSheet, View, Image, Text, TextInput, KeyboardAvoidingView, Button, TouchableNativeFeedback, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Checkbox } from 'react-native-paper';
 
 const LoginScreen = props => {
     const [isSelected, setisSelected] = useState(false);
+    const [isUrdu, setisUrdu] = useState(false);
 
     const SigninButtonHandler = () => {
         props.navigation.navigate('Rednet')
@@ -19,17 +20,72 @@ const LoginScreen = props => {
                 <Image source={require('../assets/signInLogo.png')} style={styles.image} resizeMode='contain' />
             </View>
 
-            <View style={styles.inputContainer}>
+            {isUrdu && <View style={styles.inputContainer}>
                 <View style={styles.inputField}>
-                    <View style={styles.icon}>
-                        <MaterialIcons name='account-circle' size={30} color='white' />
-                    </View>
-
                     <TextInput
                         id="phoneno"
                         label='Phone NO'
                         keyboardType='number-pad'
-                        placeholder='Enter Phone no'
+                        placeholder='فون نمبر درج کریں'
+                        style={styles.input}
+                    />
+                    <View style={styles.icon}>
+                        <MaterialIcons name='account-circle' size={30} color='white' />
+                    </View>
+                </View>
+
+                <View style={{ ...styles.inputField, ...{ marginTop: 10 } }}>
+                    <TextInput
+                        id="password"
+                        label='password'
+                        keyboardType='default'
+                        placeholder='پاس ورڈ درج کریں'
+                        secureTextEntry={true}
+                        style={{ ...styles.input, ...{ textAlign: 'right' } }}
+                    />
+                    <View style={styles.icon}>
+                        <MaterialIcons name='lock' size={30} color='white' />
+                    </View>
+                </View>
+
+                <View style={{ ...styles.rememberMe, ...{ justifyContent: 'flex-start' } }}>
+                    <Checkbox
+                        uncheckedColor='white'
+                        status={isSelected === false ? 'unchecked' : 'checked'}
+                        onPress={() => setisSelected(prevState => !prevState)}
+                    />
+                    <Text style={{ color: 'white' }}>مجھے پہچانتے ہو</Text>
+
+                </View>
+
+                <View style={styles.button} >
+                    <Button title='لاگ ان کریں' color='#181c1b' onPress={SigninButtonHandler} />
+                </View>
+                <View style={{ alignItems: 'center', marginTop: 10 }}>
+                    <Text style={{ fontSize: 25, color: 'white', textDecorationLine: 'underline' }}>پاسورڈ بھول گے؟</Text>
+                </View>
+                <View style={{ alignItems: 'center', marginTop: '15%' }}>
+                    <Text>
+                        <Text style={{ fontSize: 22, color: 'white' }}>کوئی اکاؤنٹ نہیں ہے؟ </Text>
+                        <Text style={{ fontSize: 22, color: 'white', textDecorationLine: 'underline', color: '#16e0ae' }}
+                            onPress={() => props.navigation.navigate('Signup')}
+                        >
+                            نیا بنائیں
+                        </Text>
+                    </Text>
+                </View>
+            </View>
+            }
+            {!isUrdu && <View style={styles.inputContainer}>
+                <View style={styles.inputField}>
+                    <View style={styles.icon}>
+                        <MaterialIcons name='account-circle' size={30} color='white' />
+                    </View>
+                    <TextInput
+                        id="phoneno"
+                        label='Phone NO'
+                        keyboardType='number-pad'
+                        placeholder='Enter Phone No'
                         style={styles.input}
                     />
                 </View>
@@ -38,7 +94,6 @@ const LoginScreen = props => {
                     <View style={styles.icon}>
                         <MaterialIcons name='lock' size={30} color='white' />
                     </View>
-
                     <TextInput
                         id="password"
                         label='password'
@@ -51,7 +106,6 @@ const LoginScreen = props => {
 
                 <View style={styles.rememberMe}>
                     <Text style={{ color: 'white' }}>Remember me</Text>
-                    
                     <Checkbox
                         uncheckedColor='white'
                         status={isSelected === false ? 'unchecked' : 'checked'}
@@ -60,10 +114,10 @@ const LoginScreen = props => {
                 </View>
 
                 <View style={styles.button} >
-                    <Button title='Sign in' color='#181c1b' onPress={SigninButtonHandler} />
+                    <Button title='Log in' color='#181c1b' onPress={SigninButtonHandler} />
                 </View>
                 <View style={{ alignItems: 'center', marginTop: 10 }}>
-                    <Text style={{ fontSize: 25, color: 'white', textDecorationLine: 'underline' }}>Forget Password?</Text>
+                    <Text style={{ fontSize: 25, color: 'white', textDecorationLine: 'underline' }}>Forgot Password</Text>
                 </View>
                 <View style={{ alignItems: 'center', marginTop: '15%' }}>
                     <Text>
@@ -71,10 +125,20 @@ const LoginScreen = props => {
                         <Text style={{ fontSize: 22, color: 'white', textDecorationLine: 'underline', color: '#16e0ae' }}
                             onPress={() => props.navigation.navigate('Signup')}
                         >
-                            Create one
+                            Create new
                         </Text>
                     </Text>
                 </View>
+            </View>
+            }
+            <View style={styles.languageContainer}>
+                <Text style={{ fontSize: 20, color: 'white' }}>Language: </Text>
+                <TouchableOpacity style={styles.language} onPress= {() => setisUrdu(false)} >
+                    <Text style={{ fontSize: 20, color: 'white', textDecorationLine: 'underline' }}>english</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.language} onPress= {() => setisUrdu(true)} >
+                    <Text style={{ fontSize: 20, color: 'white', textDecorationLine: 'underline' }}>اردو</Text>
+                </TouchableOpacity>
             </View>
         </KeyboardAvoidingView>
     )
@@ -83,7 +147,7 @@ const LoginScreen = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        backgroundColor: '#1d2120',
+        backgroundColor: '#1d2120'
     },
     text: {
         color: 'white'
@@ -136,6 +200,16 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         borderColor: 'white',
         borderWidth: 2
+    },
+    languageContainer: {
+        flexDirection: 'row',
+        paddingLeft: 10,
+        paddingRight: 10,
+        justifyContent: 'space-between', 
+        marginTop: 30
+    },
+    language: {
+        flexDirection: 'row'
     }
 })
 
